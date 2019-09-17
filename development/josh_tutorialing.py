34bea97d66ba5b22f7ddc500836e4ea9fa6747b6
josh <joshua.chuang21@ycdsbk12.ca>
2019-09-17 12:02:03 -0400

    arcade

---
+++ development/josh_tutorialing.py
@@ -0,0 +1,40 @@
+import arcade
+
+
+def on_update(delta_time):
+    pass
+
+
+def on_draw():
+    pass
+
+
+def on_key_press(key, modifiers):
+    pass
+
+
+def on_key_release(key, modifiers):
+    pass
+
+
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+
+def setup():
+    arcade.open_window(600, 600, "My Arcade Game")
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.schedule(on_update, 1/60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
+    window.on_mouse_press = on_mouse_press
+
+    arcade.run()
+
+
+if __name__ == '__main__':
+    setup()
